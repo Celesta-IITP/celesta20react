11e1e30127fd9aab5f813a2ac21575b6c6e6722e
sk044 <shubham_1901mm32@iitp.ac.in>
2020-11-05 20:56:15 +0530

    updated teams and added admin portal

---
+++ src/components/Team/teamdata.js
@@ -0,0 +1,264 @@
+const teamdata = [
+       
+            { 
+                "teamname" : "Fest Coordinators",
+                "members" : [{
+                        "name" : "Joshika Arya",
+                        "image" :  "https://scontent.fpat2-2.fna.fbcdn.net/v/t1.0-9/89537639_523116611944086_7782453755072479232_o.jpg?_nc_cat=100&ccb=2&_nc_sid=09cbfe&_nc_ohc=TvWpesHPR3oAX-MyMP9&_nc_ht=scontent.fpat2-2.fna&oh=11b64b5031d639e4ccb1e8ea0b88c143&oe=5FC9483B",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+
+                    },
+                    {
+                        "name" : "Yuvi Dhelawat",
+                        "image" :  "https://scontent.fpat2-1.fna.fbcdn.net/v/t1.0-9/89327184_2780341048730990_3250393591339548672_o.jpg?_nc_cat=108&ccb=2&_nc_sid=09cbfe&_nc_ohc=kg78GwRTTMMAX8Dd_-x&_nc_ht=scontent.fpat2-1.fna&oh=49b64e6cbff559686f3d90f5f9559712&oe=5FC83D41",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                ]
+
+        },
+        { 
+                "teamname" : "Marketing and Sponsorship",
+                "members" : [{
+                        "name" : "Shashwat Mahajan ",
+                        "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                    {
+                        "name" : "Aryan Kothari",
+                        "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                    {
+                        "name" : "Balbeer Yadav",
+                        "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                ]
+
+        },
+        { 
+            "teamname" : "Media and Public Relations",
+            "members" : [{
+                    "name" : "Arohan Panda",
+                    "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                    "position" : "",
+                    "fb" : "www.fb.com",
+                    "ln" : "www.linkedin.com",
+                },
+                {
+                    "name" : "Vijaya Gonugade",
+                    "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                    "position" : "",
+                    "fb" : "www.fb.com",
+                    "ln" : "www.linkedin.com",
+                },
+                {
+                    "name" : "Anmol Chaddha",
+                    "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                    "position" : "",
+                    "fb" : "www.fb.com",
+                    "ln" : "www.linkedin.com",
+                },
+                {
+                    "name" : "Kunj Taneja",
+                    "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                    "position" : "",
+                    "fb" : "www.fb.com",
+                    "ln" : "www.linkedin.com",
+                },
+            ]
+
+    },
+    { 
+        "teamname" : "Dev Team ",
+        "members" : [{
+                "name" : "Aman Jee",
+                "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                "position" : "",
+                "fb" : "www.fb.com",
+                "ln" : "www.linkedin.com",
+            },
+            {
+                "name" : "Ashwani Yadav",
+                "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                "position" : "",
+                "fb" : "www.fb.com",
+                "ln" : "www.linkedin.com",
+            },
+        ]
+
+            },
+            { 
+                "teamname" : "Registration",
+                "members" : [{
+                        "name" : "Vinay Kumar Meena",
+                        "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                    {
+                        "name" : "Rakesh Kumar",
+                        "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                    {
+                        "name" : "Swati Kumari",
+                        "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                ]
+
+            },
+            { 
+                "teamname" : "Creatives",
+                "members" : [{
+                        "name" : "Harshavardhan.k ",
+                        "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                    {
+                        "name" : "Shreyansh Anand ",
+                        "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                    {
+                        "name" : "Hareesh Tholesay",
+                        "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                ]
+
+            },
+            { 
+                "teamname" : "Hospitality & Logistics",
+                "members" : [{
+                        "name" : "Saiyyam Mittal",
+                        "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                    {
+                        "name" : "Sunny Chaturvedi",
+                        "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                    {
+                        "name" : "Rahul Verma ",
+                        "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                ]
+
+            },
+            { 
+                "teamname" : "Flagship Events",
+                "members" : [{
+                        "name" : "Hrishabh Raj",
+                        "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                    {
+                        "name" : "Durgesh Singh",
+                        "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                    {
+                        "name" : "Anuj Yadav ",
+                        "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                ]
+
+            },
+            { 
+                "teamname" : "Workshops, Exhibitions and Operations",
+                "members" : [{
+                        "name" : "Gaurav Pratap Singh",
+                        "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                    {
+                        "name" : "Aviral Agarwal",
+                        "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                    {
+                        "name" : "Ishaan Mishra",
+                        "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                ]
+
+            },
+            { 
+                "teamname" : "Events & Guest Lectures",
+                "members" : [{
+                        "name" : "Shreya Sinha",
+                        "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                    {
+                        "name" : "Akash Balaji",
+                        "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                    {
+                        "name" : "Rakesh Meena",
+                        "image" :  "https://i.ibb.co/JC4skS0/team-animate.jpg",
+                        "position" : "",
+                        "fb" : "www.fb.com",
+                        "ln" : "www.linkedin.com",
+                    },
+                ]
+
+            },
+
+]
+
+export default teamdata;
+
+
+//For image from directory use this instead of url
+// "image" :  require("../../assets/img/Sponsors/sbi.png"),
